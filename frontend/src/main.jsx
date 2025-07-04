1f3f6691ec840dff3da81cfc719b7fecc81eafb6
dggz <maks-laptev.03@mail.ru>
2025-07-05 00:23:21 +0300

    import flowbite

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./components/app/App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./context/user-context.jsx";
 import './index.css';
+import 'flowbite';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
